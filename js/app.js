954abb4a7a565449f80679c48955da56c2d88431
gilang aditya <madegilangaditya32@gmail.com>
2022-06-10 21:02:20 +0800

    finish javascript function to show the chart

---
+++ js/app.js
@@ -0,0 +1,48 @@
+function getChart() {
+    fetch('../data.json')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      let output = '';
+      let maxVal = 0;
+      const d = new Date('June 27, 2022');
+      let day = d.getDay();
+      data.forEach(function(chart){
+        output += `<div class="grafik-value">
+                        <span class="day">${chart.day}</span>
+                        <div class="grafik-chart" data-val="${chart.amount}"></div>
+                        <div class="grafik-count">$${chart.amount}</div>
+                    </div>`;
+        if(maxVal === 0){
+            maxVal = chart.amount;
+        }else{
+            if(maxVal < chart.amount){
+                maxVal = chart.amount;
+            }
+        }
+        console.log(maxVal);
+      });
+      document.querySelector('.grafiks-wrap').innerHTML = output;
+      document.querySelectorAll('.grafik-chart').forEach(function(data, index){
+        const chart = data.dataset.val;
+        let size = 150 / 100 * (chart / maxVal * 100);
+        console.log(size);
+        console.log(maxVal);
+        console.log(chart);
+        data.style.height = `${size}px`;
+        console.log(index);
+        console.log(day);
+        if(day !== 0){
+            if(day - 1   === index){
+                data.classList.add('active');
+            }
+        }else{
+            document.querySelector('.grafiks-wrap .grafik-value:last-child .grafik-chart').classList.add('active');
+        }
+        
+      });
+    })
+    .catch(err => console.log(err));
+  }
+
+  getChart();
